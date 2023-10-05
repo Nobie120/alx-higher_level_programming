1cbbabfc1e3c19469ad83b417edf3e4430616f60
Nobie120 <n12507400@gmail.com>
2023-10-05 16:46:03 -0700

    bytecode

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -4,8 +4,8 @@ def magic_calculation(a, b):
     if a < b:
         c = fn.add(a, b)
         for i in range(4, 6):
-            c = add(c, i)
+            c = fn.add(c, i)
         return c
     else:
-        return sub(a, b)
+        return fn.sub(a, b)
     return 0
