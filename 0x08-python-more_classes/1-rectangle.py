766a0c9d4b3c37ba6ab63ab2c04ba16fdd4f2900
Nobie120 <n12507400@gmail.com>
2023-10-30 15:34:57 -0700

    playing with properties

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,4 +1,39 @@
 #!/usr/bin/python3
+"""
+A class Rectangle that defines a rectangle by
+"""
+
 
 class Rectangle:
+    """ Representation of a recyangle """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
 
+    @property
+    def width(self):
+        """ getter for the width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ setter for width """
+        if width is not isinstance(int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ a getter for the height attribute """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ setter for height """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
