6524e93728171603c0260d6f2f5116e15105229c
Nobie <135202363+Nobie120@users.noreply.github.com>
2023-11-07 02:02:40 +0300

    Using upper() to call the parents __init__ method

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -7,6 +7,8 @@ class Square(Rectangle):
     '''A subclass representing a rectangle.'''
     def __init__(self, size):
         '''Constructor.'''
+        self.integer_validator("size", size)
+        self.__size = size
         super().__init__(size, size)
 
     def area(self):
