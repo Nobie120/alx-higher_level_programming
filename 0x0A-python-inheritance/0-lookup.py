e61d641ad8d8a205d86940e86ea0b4751aa79b57
Nobie120 <n12507400@gmail.com>
2023-11-06 06:33:06 -0800

    returns the list of available attributes and methods of an object:

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
+
 def lookup(obj):
     """ lookup - returns the list
     of available attributes and methods of an object:
     Args:
-        @obj:The object
-    raises:
+        obj:The object
+    Raises:
         none
     Returns:
         a list
     """
-
     return dict(obj)
