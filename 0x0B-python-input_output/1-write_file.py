bc134f5305a384fdd13d7e8bfd3005a7b7876721
Nobie120 <n12507400@gmail.com>
2023-11-07 06:17:06 -0800

    Function for writing into a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+''' Module for writing in a file '''
+
+
+def write_file(filename="", text=""):
+    '''writes into a file and overwrite the previous content
+    Args:
+        filename - name of file
+        text - what to be writen into the file
+    Returns:
+        the number of characters written:
+    '''
+    with open('filename', 'w', encoding="UTF-8") as f:
+        f.write(text)
