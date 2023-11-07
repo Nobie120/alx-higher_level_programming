5d1aa954fe6bb4ce31c94c9f9e6001e354ab569a
Nobie120 <n12507400@gmail.com>
2023-11-07 06:52:36 -0800

    function for reading and printing content of a file in stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,5 +8,4 @@ def read_file(filename=""):
         Filename-name of file
     '''
     with open(filename, 'r', encoding="UTF-8") as f:
-        for line in f:
-            print(line)
+        print(f.read(), end='')
