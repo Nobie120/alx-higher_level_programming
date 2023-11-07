dd2c638bf4ae19daa0f6e2e287418f4436d61ed2
Nobie120 <n12507400@gmail.com>
2023-11-07 06:38:03 -0800

    Function for appending content onto a file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,4 +2,11 @@
 ''' Module for appending content to a file '''
 
 
-
+def append_write(filename="", text=""):
+    ''' Appends content to a file
+    Args:
+        filename - name of the file
+        text - text to be appended
+    '''
+    with open(filename, 'a', encoding="UTF-8") as f:
+        return f.write(text)
