c7a86fb0a0443bb50b5664f24999c94962296f75
Nobie120 <n12507400@gmail.com>
2023-11-13 11:58:36 -0800

    style updates

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -3,8 +3,10 @@
 from base import Base
 
 
+
 class Rectangle(Base):
     ''' the class rectagle '''
+
     def __init__(self, width, height, x=0, y=0, id=None):
         ''' class constructor '''
 
