2de18b845296700a3b85ee87726870812d2ae48d
Nobie120 <n12507400@gmail.com>
2023-12-14 09:41:49 -0800

    please read me

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- listing privileges
-SHOW GRANTS FOR 'use_0d_1'@'localhost';
-SHOW GRANTS FOR 'use_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
