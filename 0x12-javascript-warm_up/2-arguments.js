ce1076c7504b1aebd40ff7a3cbe9eefbd6e9b63b
Nobie120 <n12507400@gmail.com>
2024-01-08 03:44:42 -0800

    use semistandard --fix to fix styling issues automatically

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -9,7 +9,7 @@ const args = process.argv;
 if (args.length < 3) {
   console.log('No argument');
 } else if (args.length === 3) {
-  console.log('Arguments found');
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
