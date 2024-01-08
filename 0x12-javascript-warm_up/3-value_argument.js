bc6d62ffcb27df9e8726395fe76063f2826dd642
Nobie120 <n12507400@gmail.com>
2024-01-08 03:53:39 -0800

    if else sattements

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+/*
+ * prints the arguements passed to it
+ */
+
+const args = process.argv;
+const arg = args[2];
+if (arg) { console.log(arg); } else { console.log('No arguement'); }
