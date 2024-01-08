7fe4a405d50e02021f923e1c7f26c0fcfa73d620
Nobie120 <n12507400@gmail.com>
2024-01-08 00:52:49 -0800

    command line arguements

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+/*
+ * prints a message depending of the number of arguments passed
+ */
+
+const args = process.argv;
+
+if (args.length < 3){
+	console.log("No argument");
+}else if (args.length === 3){
+	console.log("Arguments found");
+}else {
+	console.log("Arguments found");
+}
