16702959dc314b09081471bb210b7d79c6c5bda5
Nobie120 <n12507400@gmail.com>
2024-01-13 11:48:53 -0800

    Pattern Matching

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,12 +15,8 @@ db = MySQLdb.connect(
         port=3306
         )
 cur = db.cursor()
-cur.execute(
-    """ 
-    SELECT * FROM states WHERE name LIKE BINARY 'N%'
-    ORDER BY id ASC
-    """
-    )
+cur.execute("""SELECT * FROM states WHERE name
+        LIKE BINARY 'N%' ORDER BY id ASC""")
 result = cur.fetchall()
 for row in result:
     print(row)
