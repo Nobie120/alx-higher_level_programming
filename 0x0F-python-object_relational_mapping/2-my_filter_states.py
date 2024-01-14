370d10e2e42e7f17207950f86a2c5ac007fb43d6
Nobie120 <n12507400@gmail.com>
2024-01-14 07:30:22 -0800

    patern search

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -14,7 +14,9 @@ if __name__ == "__main__":
             host="localhost", user=sys.argv[1],
             passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name='{}' ORDER BY id ASC".format(sys.argv[4]))
+    cur.execute(
+            """SELECT * FROM states WHERE name LIKE BINARY '{}'
+            ORDER BY id ASC""".format(sys.argv[4]))
     result = cur.fetchall()
 
     for row in result:
