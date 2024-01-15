3cfaeb01a5cb34c3b79d283a3a0ce02d01923b08
Nobie120 <n12507400@gmail.com>
2024-01-14 23:52:30 -0800

    INNER JOIN

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -14,8 +14,7 @@ if __name__ == "__main__":
             db=sys.argv[3], port=3306)
     cur = db.cursor()
     cur.execute("""SELECT cities.id, cities.name, states.name FROM
-                cities INNER JOIN states ON states.id=cities.state_id
-                ORDER BY cities.id ASC""")
+                cities INNER JOIN states ON states.id=cities.state_id""")
     rows = cur.fetchall()
     for row in rows:
         print(row)
