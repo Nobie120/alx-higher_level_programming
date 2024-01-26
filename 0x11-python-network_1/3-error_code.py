7d78851dc548a5de56efa989caf3ee0064c07d9c
Nobie120 <n12507400@gmail.com>
2024-01-26 14:46:57 -0800

    handling errors

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,7 +4,12 @@ ends a request to the URL and displays
 the body of the response (decoded in utf-8)
 """
 import sys
-import urllib.request
+from urllib import request, error
 
 
 if __name__ == "__main__":
+    try:
+        with request.urlopen(sys.argv[1]) as response:
+            print(response.read().decode("utf-8"))
+    except error.HTTPError as e:
+        print(f"Error code: {e.code}")
