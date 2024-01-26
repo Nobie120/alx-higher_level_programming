5e6616418c3a6aadf19869bdb4085c94195b7e1b
Nobie120 <n12507400@gmail.com>
2024-01-26 14:30:53 -0800

    POST

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -13,5 +13,5 @@ if __name__ == "__main__":
 
     data = urllib.parse.urlencode({"email": email}).encode('utf-8')
 
-    with urllib.request.urlopen(url, data=data) as response:
+    with urllib.request.urlopen(url, data) as response:
         print(responce.read().decode('utf-8'))
