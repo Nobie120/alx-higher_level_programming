70eefa5c3f0b7a55531d0dc6f72a9cf4bbd84016
Nobie120 <n12507400@gmail.com>
2024-02-20 12:56:08 -0800

    star wars API

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -6,7 +6,7 @@ const url = 'https://swapi-api.hbtn.io/api/films/';
 request.get(url + movieId, (error, response, body) => {
   if (error) {
     console.log(error);
-  } else if (response.statusCode == 200) {
+  } else if (response.statusCode === 200) {
     const jsonBody = JSON.parse(body);
     console.log(jsonBody.title);
   } else {
