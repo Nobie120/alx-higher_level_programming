26923f2a8989ebca556b66354c444b897550fae9
Nobie <135202363+Nobie120@users.noreply.github.com>
2024-03-11 11:49:03 +0300

    About to start The Javascript warmup again!!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-if (process.argv.length < 4) {
-  console.log('0');
-} else {
-  const arr = process.argv.slice(2).map(Number);
-  const second = arr.sort((a, b) => b - a);
-  console.log(second[1]);
-}
--- 0x12-javascript-warm_up/3-value_argument.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-/*
- * prints the arguements passed to it
- */
-
-const args = process.argv;
-const arg = args[2];
-if (arg) { console.log(arg); } else { console.log('No argument'); }
