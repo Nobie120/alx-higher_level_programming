6beb73434bd90a461047c000e31b89efa5e16186
Nobie120 <n12507400@gmail.com>
2024-03-11 14:04:31 -0700

    recursion

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 
 const factorize = (n) => {
-  if (isNaN(n)) {
+  if (isNaN(n)i || n === 0) {
     return 1;
   }
-  if (n <= 1) {
-    return 1;
+  if (n < 0) {
+    return -1;
   } else {
     return n * factorize(n - 1);
   }
 };
 
 const n = parseInt(process.argv[2]);
-console.log(factorize(parseInt(n));
+console.log(n);
