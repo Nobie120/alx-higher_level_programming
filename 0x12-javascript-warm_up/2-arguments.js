1d674a34095fbf1ecf63a7be3e4c73f40202f537
Nobie120 <n12507400@gmail.com>
2024-03-11 11:02:52 -0700

    cmd arguements

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 
-let args = process.argv.slice(2);
+const args = process.argv.slice(2);
 
 if (args.length === 0) {
   console.log('No argument');
-}
-else if(args.length === 1) {
+} else if (args.length === 1) {
   console.log('Argument found');
-}
-else {
+} else {
   console.log('Arguments found');
 }
