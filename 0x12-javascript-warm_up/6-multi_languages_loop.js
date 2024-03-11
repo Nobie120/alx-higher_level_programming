d20bbc26186e396aff01fc6b4d39b333c96c2725
Nobie120 <n12507400@gmail.com>
2024-03-11 12:00:41 -0700

    loops

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const langs = ['C is fun', 'Python is coo', 'JavaScript is amazing'];
+const langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (i = 0; i < langs.length; i++) {
+for (let i = 0; i < langs.length; i++) {
   console.log(langs[i]);
 }
