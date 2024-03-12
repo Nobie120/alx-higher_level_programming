c728db4fead2c67312fe913db701c4a968577ed2
Nobie120 <n12507400@gmail.com>
2024-03-11 17:22:19 -0700

    dictionary

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,15 +1,12 @@
 #!/usr/bin/node
-
-const factorize = (n) => {
-  if (isNaN(n)i || n === 0) {
-    return 1;
-  }
+function factorize (n) {
   if (n < 0) {
     return -1;
-  } else {
-    return n * factorize(n - 1);
   }
-};
+  if (isNaN(n) || n === 0) {
+    return 1;
+  }
+  return n * factorize(n - 1);
+}
 
-const n = parseInt(process.argv[2]);
-console.log(n);
+console.log(factorize(Number(process.argv[2])));
