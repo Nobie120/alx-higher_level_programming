f5ef0f5ea3cd8f59a07738de35ce979e5a6db0c0
Nobie120 <n12507400@gmail.com>
2024-03-12 08:02:54 -0700

    method is an attribute

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 class Rectangle {
-  construct (w, h) {
+  constructor (w, h) {
     if ((w > 0) && (h > 0)) {
-      this.weight = w;
+      this.width = w;
       this.height = h;
     }
   }
