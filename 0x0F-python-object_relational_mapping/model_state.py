22a325422c1f9a054b96bc888dc486c03a44e89b
Nobie120 <n12507400@gmail.com>
2024-03-17 04:32:54 -0700

    declarative_base is the key

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!/usr/biin/python3
+"""the class definition of a State and
+an instance Base = declarative_base()"""
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """
+    Class with id and name attributes of each state
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, nullable=False, unique=True, primary_key=True)
+    name = Column(String(128), nullable=False)
