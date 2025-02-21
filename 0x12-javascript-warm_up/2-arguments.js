8c2993da0acc03862e972459e191cd7177c0afd5
Nobie120 <n12507400@gmail.com>
2025-02-21 04:15:30 -0800

    command line args

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,10 +2,8 @@
 
 const args = process.argv.slice(2);
 
-if (args.length === 0) {
-  console.log('No argument');
-} else if (args.length === 1) {
-  console.log('Argument found');
-} else {
-  console.log('Arguments found');
+if (args < 2) {
+	console.log("No arguement")
+}else {
+	console.log(args[0])
 }
