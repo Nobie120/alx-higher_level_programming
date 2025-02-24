89ecfdb5abf50f3d261fb54eb7abe0262b76a52e
Nobie120 <n12507400@gmail.com>
2025-02-24 10:50:21 -0800

    recurson

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+function fact(n) {
+
+	if (isNaN(n) || n < 0) {
+		return -1;
+	}else if (n === 0 || n === 1) {
+		return 1;
+	} else {
+		return n * fact(n - 1);
+	}
+}
+
+console.log(fact(parseInt(process.argv[2])));
